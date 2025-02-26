e30e093324a1e8eeb823a8c40803e2a231eb9602
Kamal Preet <kamalpreet82746@gmail.com>
2025-02-26 23:02:26 +0530

    Product model done

---
+++ models/product.model.js
@@ -0,0 +1,25 @@
+import  mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+
+    name:{
+        type : String,
+        required : true,    
+    },
+    price:{
+        type : String,
+        required : true,
+    },
+    description:{
+        type : String,
+        default: "This is the best product",
+    },
+    category: [],
+  },
+  { timestamps: true }
+);
+
+
+const Product = mongoose.model("Product",productSchema)
+
+export default Product;
