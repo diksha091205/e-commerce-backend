6ec6c7aa9d5749572bcd3d449f1d18cc74d5a570
Kamal Preet <kamalpreet82746@gmail.com>
2025-02-27 23:03:14 +0530

    user route created

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 import db from "./db.js"
-
+import User from "./models/users.model.js"; 
 import express from "express"
 // const db = require("./db") 
 
 // const express = require("express")
 
 const app = express()
+
+app.use(express.json())
+
+
 db();
 app.get("/",(req, res) => {
     res.send("Hello Diksha")
     })
+ 
+    app.post("/user" , async (req,res)=>{
+        const user = new User(req.body)
+        try {
+            await user.save()
+            res.status(201).send({user})
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    })
+    
 
-
-    app.listen(3000,()=>{
+ app.listen(3000,()=>{
 console.log("server is running")
 })
